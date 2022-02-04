7e4d29a17b66407830b06038891912e7ace304e5
Daprojects1 <93226161+Daprojects1@users.noreply.github.com>
2022-02-04 22:07:59 +0000

    Added a calculator class for mor reusablecode

--- script.js
+++ script.js
@@ -2,25 +2,31 @@ let calcBody = document.querySelector(".calc-body")
 let calcHead = document.querySelector(".calc-head")
 let mainbts = document.querySelector(".mainbtns")
 let plus = document.querySelector(".plus")
-let obj = [0,1,2,3,4,5,6,7,8,9]
 let equals = document.querySelector(".equals")
 let clear = document.querySelector(".clear")
-let maths = [
-    { 
-      mulitiply(a,b){
-          return a*b
-      },
-      divide(a,b){
-        return a/b;
-      },
-      add(a,b){
-        return a+b 
-      },
-      subtract(a,b){
-         return a-b
-      }
+
+let obj = [0,1,2,3,4,5,6,7,8,9]
+
+
+class Calculator {
+    constructor(a, b){
+        this.a = a;
+        this.b = b;
     }
-] 
+    multiply(){
+        return this.a*this.b
+    }
+    divide(){
+        return this.a/this.b;
+      }
+      add(){
+        return this.a+this.b 
+      }
+      subtract(){
+        return this.a-this.b;
+     }
+}
+
 const createBody = ()=>{
     obj.forEach((item, index)=>{
         let newbtn = document.createElement("div")
@@ -32,28 +38,25 @@ const createBody = ()=>{
     })
 }
 
-
-let a = "";
-let b = ""; 
+let first= [] ;
+let a ;
+let b; 
 const addNum= (e)=>{
+    calcHead.innerHtml
     let firstVal = ()=>{
-        a += e.target.getAttribute("data-id");
-        calcHead.innerText = Number(a);
-        return Number(a);
+        first.push ( e.target.getAttribute("data-id"));
+        a = first.join();
+         calcHead.innerHTML = first.join("");
+         return a;
     }
     firstVal();
-    let SecondVal = ()=>{
-        b += e.target.getAttribute("data-id")
-        calcHead.innerText = Number(b)
-    }
-    plus.addEventListener("click",()=>{
-         calcHead.innerText = "";
-         SecondVal();
+    plus.addEventListener("click", ()=>{
+       first.length = 0;
+       b = first.join();
+       calcHead.innerHTML = first.join("");
+       return b;
     })
-    
+     
 }
 
-const funcs = ()=>{
-}
 createBody();
-funcs();
