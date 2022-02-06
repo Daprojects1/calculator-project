8e555a2f4c9d28ede76a35744d3ae9fbc69ae771
Daprojects1 <93226161+Daprojects1@users.noreply.github.com>
2022-02-06 17:05:20 +0000

    still working on functionality

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let calcBody = document.querySelector(".calc-body")
 let calcHead = document.querySelector(".calc-head")
 let mainbts = document.querySelector(".mainbtns")
-let plus = document.querySelector(".plus")
+let plus = document.querySelector("#add")
 let equals = document.querySelector(".equals")
 let clear = document.querySelector(".clear")
+let numbers = document.querySelector(".otherBody")
 
-let obj = [0,1,2,3,4,5,6,7,8,9]
 
 
 class Calculator {
@@ -27,58 +27,98 @@ class Calculator {
      }
 }
 
-const createBody = ()=>{
-    obj.forEach((item, index)=>{
-        let newbtn = document.createElement("div")
-        newbtn.setAttribute("data-id", index)
-        newbtn.innerText = index;
-        newbtn.classList.add("icons")
-        calcBody.appendChild(newbtn)
-        newbtn.addEventListener("click", firstVal)
-    })
-}
-
-<<<<<<< HEAD
-
+let m = [];
+let c = [];
 let a = "";
-let b = ""; 
-
-
-let secondVal = (a)=>{
-    let clear = ()=>{
-        calcHead.innerText= "";
-    }
-   plus.addEventListener("click", clear )
-   console.log(calcHead.innerText)
+let b = [];
+let counter = 0;
+const showFirstVal = (e)=>{
+  let mainVal = ()=>{
+      c.push(e.target.getAttribute("data-id"))
+      calcHead.innerHTML= c.join("");
+      a = c.join("")
+  }
+ mainVal();
 }
 
-let firstVal = (e)=>{
-        a += e.target.getAttribute("data-id");
-        calcHead.innerText = Number(a);
-       secondVal(a)
- }
 
-=======
-let first= [] ;
-let a ;
-let b; 
-const addNum= (e)=>{
-    calcHead.innerHtml
-    let firstVal = ()=>{
-        first.push ( e.target.getAttribute("data-id"));
-        a = first.join();
-         calcHead.innerHTML = first.join("");
-         return a;
-    }
-    firstVal();
-    plus.addEventListener("click", ()=>{
-       first.length = 0;
-       b = first.join();
-       calcHead.innerHTML = first.join("");
-       return b;
-    })
-     
+let secondVal =(e)=>{
+  calcHead.innerHTML=""
+  c.length =0;
+   b.shift()
+  b.push(a)
 }
 
->>>>>>> 7e4d29a17b66407830b06038891912e7ace304e5
-createBody();
+ plus.addEventListener("click", (e)=>{
+   secondVal(e);
+   
+ })
+
+// const equalEd = ()=>{
+
+// }
+
+const numbs = ()=>{
+for (let i =0; i < numbers.children.length; i++){
+    numbers.children[i].setAttribute("data-id", i)
+     numbers.children[i].addEventListener("click", showFirstVal)
+}
+}
+
+numbs();
+
+
+// const createBody = ()=>{
+//     obj.forEach((item, index)=>{
+//         let newbtn = document.createElement("div")
+//     
+//         newbtn.innerText = index;
+//         newbtn.classList.add("icons")
+//         calcBody.appendChild(newbtn)
+//         newbtn.addEventListener("click", addNum)
+//     })
+// }
+
+
+// let first= [];
+// let a ;
+// let b; 
+// let counter = 1;
+
+// const addNum= (e)=>{
+//     let firstVal = ()=>{
+//         if (counter > 0 && counter % 2 !== 0){
+//             first.push ( e.target.getAttribute("data-id"));
+//             a = first.join("");
+//              calcHead.innerHTML = a;
+//             return a;
+//         } 
+//         }
+//         if (counter > 0 && counter % 2 === 0){
+//             first.push(e.target.getAttribute("data-id"))
+//             b = first.join("");
+//             calcHead.innerHTML = b;
+//             return b;
+//         }       
+//     firstVal();     
+// }
+
+
+// let equal = (a,b)=>{
+//     console.log(g)
+//     equals.addEventListener("click",()=>{
+//      let m = new Calculator(Numa,Numb)
+//      calcHead.innerHTML =m.add();
+//     })
+    
+// }
+// let c =   ()=>{
+//     plus.addEventListener("click", ()=>{
+//         first.length = 0;
+//         counter++
+//      })
+// }
+
+// c();
+// equal();
+// createBody();
